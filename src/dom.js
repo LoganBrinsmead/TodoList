6e8b4c883616fb3e9cf42fc1a6cd0465aafb5a37
Logan <loganbrinsmead1115@gmail.com>
2022-01-28 12:57:25 -0800

    Some changes to logic and styles.

--- src/dom.js
+++ src/dom.js
@@ -1,3 +1,45 @@
-// This module is meant for manipulating the DOM elements of the page
+// // This module is meant for manipulating the DOM elements of the page
 
-// export { createTask };
+// function taskDOM(obj) {
+//     // create a task list item in memory, give it a class of task
+//     const task = document.createElement('li');
+//     task.classList.add('task');
+
+//     // create the checkbox, give it complete class
+//     const checkbox = document.createElement('input');
+//     checkbox.type = 'checkbox';
+//     checkbox.classList.add('complete');
+
+//     // create div for the name of the task, give it the taskName class
+//     const taskName = document.createElement('div');
+//     taskName.textContent = obj.title;
+
+//     // create the edit button, give it the editTask class
+//     const editButton = document.createElement('button');
+//     editButton.textContent = 'Edit';
+//     editButton.classList.add('editTask');
+
+//     // create the delete task button, give it the deleteTask class
+//     const deleteButton = document.createElement('button');
+//     deleteButton.textContent = 'Delete';
+//     deleteButton.classList.add('deleteTask');
+
+//     // append the elements
+//     task.appendChild(checkbox);
+//     task.appendChild(taskName);
+//     task.appendChild(editButton);
+//     task.appendChild(deleteButton);
+
+//     return task;
+// }
+
+// // function to clear the DOM 
+// function clearDOM(selector) {
+//     const content = document.querySelector(`'${selector}'`);
+//     while (content.firstChild) {
+//         content.removeChild(content.lastChild);
+//     }
+// }
+
+
+// export { taskDOM, clearDOM };
--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ console.log("Hello, World?");
 
 // This module is for the logic of the To-Do List
 // i.e. creating To-Do List item factory functions, saving the items locally, utilizing the date-fns, etc.
-import {format, formatDistance, formatRelative, subDays} from 'date-fns';
+// import {format, formatDistance, formatRelative, subDays} from 'date-fns';
+// import { taskDOM, clearDOM } from './dom';
 
 
 ////// modal box code //////
@@ -45,32 +46,6 @@ function closeModal(modal) {
 ////// modal box code //////
 
 
-
-// function to clear the DOM 
-function clearDOM(selector) {
-    const content = document.querySelector(`'${selector}'`);
-    while (content.firstChild) {
-        content.removeChild(content.lastChild);
-    }
-}
-
-// factory function for creating task in the DOM
-// takes necessary info from the task factory function
-function createTaskObj() {
-        const _taskTitleInput = document.getElementById('taskTitle');
-        const _dueDateInput = document.getElementById('dueDate');
-        const _description = document.getElementById('taskDescription');
-
-        return newTask;
-}
-
-function taskDOM(obj) {
-        // create a task list item in memory
-        const task = document.createElement('li');
-
-         
-}
-
 function date() {
     // the current date 
     const date = new Date();
@@ -78,30 +53,110 @@ function date() {
     return date;
 }
 
-let _priority;
-function clickEvent(index) {return _priority = index};
-document.querySelectorAll('.priority').forEach(function(button, index) {
-        button.addEventListener('click', clickEvent.bind(null, index));   
-});
-document.getElementById('confirm').addEventListener('click', () => {
-        const newTask = task(
-                _taskTitleInput.value,
-                _dueDateInput.value,
-                _priority,
-                _description.value
-        );
-        return newTask;
-});
+// module for affecting the task objects' logic
+const taskLogic = (() => {
+
+        function buttonLogic() {
+                // logic for the delete buttons on each task
+                document.querySelectorAll('.deleteTask').addEventListener('click', () => {
+                        // delete the DOM of the task using the clearDOM function
+                        // first find the object by sellecting its index
+
+                        // delete object from the array using splice
+
+                        // decrement the index of each object in the array using forEach
+                        _taskListArray.forEach(task => {
+                                task.index -= 1;
+                        });
+                });
+
+        }
+
+        function createTask(obj) {
+                // create a task list item in memory, give it a class of task
+                const task = document.createElement('li');
+                task.classList.add('task');
+                
+                // create the checkbox, give it complete class
+                const checkbox = document.createElement('input');
+                checkbox.type = 'checkbox';
+                checkbox.classList.add('complete');
+                
+                // create div for the name of the task, give it the taskName class
+                const taskName = document.createElement('div');
+                taskName.textContent = obj.title;
+                
+                // create the edit button, give it the editTask class
+                const editButton = document.createElement('button');
+                editButton.textContent = 'Edit';
+                editButton.classList.add('editTask');
+                
+                // create the delete task button, give it the deleteTask class
+                const deleteButton = document.createElement('button');
+                deleteButton.textContent = 'Delete';
+                deleteButton.classList.add('deleteTask');
+                
+                // append the elements
+                task.appendChild(checkbox);
+                task.appendChild(taskName);
+                task.appendChild(editButton);
+                task.appendChild(deleteButton);
+                
+                return task;
+        }
+        
+        // function to clear the DOM 
+        function clearDOM(selector) {
+                const content = document.querySelector(`${selector}`);
+                while (content.firstChild) {
+                        content.removeChild(content.lastChild);
+                        }
+        }
+            
+
+        // array of task objects
+        let _taskListArray = [];
+
+        // factory function to create the To-Do objects
+        /**
+        * @param {integer} index - Index of object in array of objects
+        */ 
+        const _task = (title, dueDate, priority, description, index) => {
+                // variable to see if the task has been completed
+                let _completed = false;         // if true, object is given id 'completed' to show it is completed
+                return {
+                        title,
+                        dueDate,
+                        priority,
+                        description,
+                        index,
+                        _completed
+                };
+        };
+
+        const taskTitleInput = document.getElementById('taskTitle');
+        const dueDateInput = document.getElementById('dueDate');
+        const description = document.getElementById('taskDescription');
+
+        let _priority;
+        let _index = -1;                // passed thru factory function, incremented with each added task
+        function clickEvent(index) {return _priority = index};
+        document.querySelectorAll('.priority').forEach(function(button, index) {
+                button.addEventListener('click', clickEvent.bind(null, index));   
+        });
+        document.getElementById('confirm').addEventListener('click', () => {
+                _index += 1;
+                let newTask = _task(
+                        taskTitleInput.value,
+                        dueDateInput.value,
+                        _priority,
+                        description.value,
+                        _index
+                );
+                _taskListArray = [newTask];
+                document.querySelector('.taskList').appendChild(createTask(_taskListArray[newTask]));
+        });
+})();
 
 
-// factory function to create the To-Do objects
-const task = (title, dueDate, priority, description) => {
-    return {
-        title,
-        dueDate,
-        priority,
-        description
-    };
-};
-
-createTask();
+taskLogic;
